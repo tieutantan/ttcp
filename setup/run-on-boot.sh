377dd2d1fdd1ce244f24a96661a804640dd6b550
tan-NMS <info@tantn.com>
2023-05-12 08:36:05 +0700

    update readme.md + run-on-boot.sh

--- setup/run-on-boot.sh
+++ setup/run-on-boot.sh
@@ -5,11 +5,12 @@ base_dir=$(dirname "$(pwd)")
 path="$base_dir/Node-Multiple-Simple/auto-run.sh"
 
 if ! test -f "$path"; then
+  echo "Error: $path does not exist. Code execution stopped."
+  exit 1
+fi
 
-  echo "#!/bin/bash" > "$path"
-  chmod a+x "$path"
-  if ! crontab -l | grep -q "@reboot $path"; then
-      (crontab -l; echo "@reboot $path") | crontab -
-  fi
-
+echo "#!/bin/bash" > "$path"
+chmod a+x "$path"
+if ! crontab -l | grep -q "@reboot $path"; then
+  (crontab -l; echo "@reboot $path") | crontab -
 fi
