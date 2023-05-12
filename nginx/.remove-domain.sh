77df3a612dc2ce7801346c8446b4abcfaa2e5b74
tan-NMS <info@tantn.com>
2023-05-12 08:41:22 +0700

    Update add + list + remove domain

--- nginx/.remove-domain.sh
+++ nginx/.remove-domain.sh
@@ -1,29 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
-# Check if the domain argument is provided
-if [ -z "$1" ]
-then
+# Function to display usage and exit
+display_usage() {
     echo "NMS: remove-domain usage: $0 domain"
     exit 1
+}
+
+# Check if the domain argument is provided
+if [ -z "$1" ]; then
+    display_usage
 fi
 
 # Set the domain variable
-domain=$1
+domain="$1"
 
 # Change to the directory containing the files
 folder_path="/etc/nginx/conf.d/"
 
 # Change to the folder
-cd "$folder_path"
+cd "$folder_path" || exit 1
 
 # Loop through all the files in the directory
-for file in *
-do
-    # Check if the file name contains "example.com"
-    if [[ $file == *"$domain"* ]]
-    then
+for file in *; do
+    # Check if the file name contains the domain
+    if [[ $file == *"$domain"* ]]; then
         # Remove the file
-        rm -f $file
+        rm -f "$file"
     fi
 done
 
