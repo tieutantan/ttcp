106f536671134a0c2c321669ca5b2c124906d7e1
tieutantan <info@tantn.com>
2024-10-28 14:54:39 +0700

    1

--- draft/create_check_deploy.sh
+++ draft/create_check_deploy.sh
@@ -8,17 +8,18 @@ fi
 # Example command: bash create_check_deploy.sh /path/to/app
 
 APP_PATH="$1"
-CHECK_DEPLOY_SCRIPT="$APP_PATH/check_deploy.sh"
+CHECK_DEPLOY_SCRIPT_PATH="$APP_PATH/check_deploy.sh"
 EXAMPLE_SCRIPT="example_check_deploy.sh"
 
 # Copy the content from the example_check_deploy.sh file to the check_deploy.sh script
-cp "$EXAMPLE_SCRIPT" "$CHECK_DEPLOY_SCRIPT"
+cp "$EXAMPLE_SCRIPT" "$CHECK_DEPLOY_SCRIPT_PATH"
 
 # Make the script executable
-chmod +x "$CHECK_DEPLOY_SCRIPT"
+chmod +x "$CHECK_DEPLOY_SCRIPT_PATH"
 
-# update CHECK_DEPLOY_SCRIPT to ttcp_check_deploys.sh REPO_DIRS
-sed -i "s|REPO_DIRS=(|REPO_DIRS=(\n    \"$APP_PATH\"|g" ttcp_check_deploys.sh
-
-
-echo "Created $CHECK_DEPLOY_SCRIPT"
+# update add $CHECK_DEPLOY_SCRIPT_PATH to  REPO_DIRS in ttcp_check_deploys.sh
+TTCP_CHECK_DEPLOYS_SCRIPT_PATH="ttcp_check_deploys.sh"
+REPO_DIRS_LINE=$(grep -n "REPO_DIRS=(" "$TTCP_CHECK_DEPLOYS_SCRIPT_PATH" | cut -d: -f1)
+REPO_DIRS_LINE=$((REPO_DIRS_LINE+1))
+sed -i "${REPO_DIRS_LINE}a \"$APP_PATH\"" "$TTCP_CHECK_DEPLOYS_SCRIPT_PATH"
+# doing to this line without test above code
